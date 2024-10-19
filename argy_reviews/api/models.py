14baf7a25a861b14f4fa600d8fb697c8f427559b
Facuterremoto <facuterremoto04@gmail.com>
2024-10-19 18:15:12 -0300

    Refactorizacion de algunos de los endpoints de la API

--- argy_reviews/api/models.py
+++ argy_reviews/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User #This is the default user model provided by Django
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -10,13 +11,19 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     code = models.CharField(max_length=10)
     avg_ratings = models.FloatField(default=0)
-    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
-
+    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE,blank=True, null=True)
+    image_url = models.URLField(max_length=200, null=True, blank=True)
+    
     def __str__(self):
         return self.title
 
     def get_reviews(self):
         return Review.objects.filter(post=self)
+    
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.owner_id:
+            self.owner = get_user_model().objects.get(pk=kwargs.pop('user_id'))
+        super().save(*args, **kwargs)
 
     def update_avg_ratings(self):
         ratings = self.get_ratings()
@@ -30,8 +37,12 @@ class Review(models.Model):
     rating = models.IntegerField(default=1)
     comment = models.TextField(null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    owner = models.ForeignKey('auth.User', related_name='reviews', on_delete=models.CASCADE)
-
+    owner = models.ForeignKey('auth.User', related_name='reviews', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.owner.username
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.owner_id:
+            self.owner = get_user_model().objects.get(pk=kwargs.pop('user_id'))
+        super().save(*args, **kwargs)
