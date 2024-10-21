661031b3514edbff41745cc48f3553163a0c879d
Facuterremoto <facuterremoto04@gmail.com>
2024-10-21 00:35:57 -0300

    Mejore los serializadores y modelos - Actualizacion automatica del promedio de los post en el CRUD - Generacion automatica de codigos alfanumericos

--- argy_reviews/api/models.py
+++ argy_reviews/api/models.py
@@ -2,26 +2,43 @@ from django.db import models
 from django.contrib.auth.models import User #This is the default user model provided by Django
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+import string
+import random
 
 # Create your models here.
 
 # TODO 
-# - [] Create a model for the Report
+# - [x] Create a model for the Report
 # - [] Makle a way to report a post, a review or a user
 # - [] Create a view to list all the reports
 # - [] Create a model for Category
 # - [] Create a category relation with the Post model
-# - [] Make the code of all the models that have coude automatically generated
+# - [x] Make the code of all the models that have coude automatically generated
 # - [] Make a way to store images in the Post model
+# - [] Make the avg_rating automatically updated when a review is created, updated or deleted
 
 class Post(models.Model):
+    code = models.CharField(max_length=10, unique= True, blank=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    code = models.CharField(max_length=10)
-    avg_ratings = models.FloatField(default=0)
+    avg_ratings = models.FloatField(default=0, blank=True)
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE,blank=True, null=True)
     image_url = models.URLField(max_length=200, null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        # Generación del código si no está presente
+        if not self.code:
+            self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        
+        # Asignación del owner si no está presente y si se proporciona user_id
+        if not self.pk and not self.owner:
+            user_id = kwargs.pop('user_id', None)
+            if user_id:
+                self.owner = get_user_model().objects.get(pk=user_id)
+
+        # Llamada al método save() del padre para guardar los cambios
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.title
@@ -29,10 +46,6 @@ class Post(models.Model):
     def get_reviews(self):
         return Review.objects.filter(post=self)
     
-    def save(self, *args, **kwargs):
-        if not self.pk and not self.owner_id:
-            self.owner = get_user_model().objects.get(pk=kwargs.pop('user_id'))
-        super().save(*args, **kwargs)
 
     def update_avg_ratings(self):
         ratings = self.get_ratings()
@@ -41,23 +54,33 @@ class Post(models.Model):
             self.save()
 
 class Review(models.Model):
-    code = models.CharField(max_length=10, default='0000000000')
-    created_at = models.DateTimeField(default=timezone.now)
-    rating = models.IntegerField(default=1)
-    comment = models.TextField(null=True)
+    code = models.CharField(max_length=10, unique= True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(null=False, blank=False)
+    comment = models.TextField(null=True, blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     owner = models.ForeignKey('auth.User', related_name='reviews', on_delete=models.CASCADE, blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+        # Generación del código si no está presente
+        if not self.code:
+            self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        
+        # Asignación del owner si no está presente y si se proporciona user_id
+        if not self.pk and not self.owner:
+            user_id = kwargs.pop('user_id', None)
+            if user_id:
+                self.owner = get_user_model().objects.get(pk=user_id)
 
+        # Llamada al método save() del padre para guardar los cambios
+        super().save(*args, **kwargs)
+        
     def __str__(self):
         return self.owner.username
 
-    def save(self, *args, **kwargs):
-        if not self.pk and not self.owner_id:
-            self.owner = get_user_model().objects.get(pk=kwargs.pop('user_id'))
-        super().save(*args, **kwargs)
 
 class Report(models.Model):
-    code = models.CharField(max_length=10, default='0000000000')
+    code = models.CharField(max_length=10, unique= True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
     content = models.TextField()
     owner = models.ForeignKey('auth.User', related_name='reports', on_delete=models.CASCADE, blank=True, null=True)
@@ -67,6 +90,15 @@ class Report(models.Model):
         return self.owner.username
 
     def save(self, *args, **kwargs):
-        if not self.pk and not self.owner_id:
-            self.owner = get_user_model().objects.get(pk=kwargs.pop('user_id'))
+        # Generación del código si no está presente
+        if not self.code:
+            self.code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        
+        # Asignación del owner si no está presente y si se proporciona user_id
+        if not self.pk and not self.owner:
+            user_id = kwargs.pop('user_id', None)
+            if user_id:
+                self.owner = get_user_model().objects.get(pk=user_id)
+
+        # Llamada al método save() del padre para guardar los cambios
         super().save(*args, **kwargs)
--- argy_reviews/api/serializers.py
+++ argy_reviews/api/serializers.py
@@ -5,6 +5,7 @@ A serializer is a class that converts complex data types, such as querysets and 
 
 from rest_framework import serializers
 from .models import * #Models are necesary to be imported in order to create the serializers
+from django.db.models import Avg
 from django.contrib.auth.models import User #This is the default user model provided by Django
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,8 +30,46 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at', 'code', 'avg_ratings', 'owner', 'image_url']
+        
+        # This refactorization of the create method is to make the code generation of the post code automatic
+    def create(self, validated_data):
+        if 'code' not in validated_data or not validated_data['code']:
+            validated_data['code'] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        return super().create(validated_data)
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['id', 'code', 'created_at', 'rating', 'comment', 'owner']
+    
+    def create(self, validated_data):
+        if 'code' not in validated_data or not validated_data['code']:
+            validated_data['code'] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+            
+        review =  super().create(validated_data)
+        self.update_post_avg_rating(review.post)
+        return review
+    
+    def update(self, instance, validated_data):
+        post = instance.post  # Guardar el post antes de actualizar
+        instance = super().update(instance, validated_data)
+        self.update_post_avg_rating(post)
+        return instance
+    
+    def update_post_avg_rating(self, post):
+        reviews = post.review_set.all()
+        if reviews.exists():
+            post.avg_ratings = reviews.aggregate(Avg('rating'))['rating__avg']
+        else:
+            post.avg_ratings = 0
+        post.save()
+    
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = ['id', 'code', 'created_at', 'content']
+    
+    def create(self, validated_data):
+        if 'code' not in validated_data or not validated_data['code']:
+            validated_data['code'] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        return super().create(validated_data)
