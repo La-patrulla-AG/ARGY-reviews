08b016d74b57dfee681721b9edcd78c27f7cf042
Manuel Rebol <manuel.rebol0103@gmail.com>
2024-12-21 15:31:10 -0300

    Theme Selector refactor

--- argy_reviews/frontend/src/components/MainLayout.jsx
+++ argy_reviews/frontend/src/components/MainLayout.jsx
@@ -1,38 +1,41 @@
 import Aside from "./ui/Aside";
 import React from "react";
-import HomePage from "./HomePage";
 import { useState, useEffect } from "react";
 import { Moon, Sun } from "lucide-react";
 import Header from "./ui/Header";
 
-const MainLayout = ({ children }) => {
-  const [modoOscuro, setModoOscuro] = useState(true);
+const MainLayout = ({ children }) => (
+  <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex flex-grow text-color">
+    <Aside />
+    <main className={`flex-1 p-4 transition-all duration-300 ml-16`}>
+      <Header />
+      {children}
+    </main>
+    <ThemeSelector />
+  </div>
+);
 
-  const toggleModoOscuro = () => setModoOscuro(!modoOscuro);
+export const ThemeSelector = () => {
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") ?? "dark");
+
+  const isDark = theme === "dark"
 
   useEffect(() => {
-    document.documentElement.classList.toggle("dark", modoOscuro);
-  }, [modoOscuro]);
+    document.documentElement.classList.toggle("dark", isDark);
+    localStorage.setItem("theme", theme)
+  }, [theme, isDark]);
 
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex flex-grow text-color">
-      <Aside />
-      <main className={`flex-1 p-4 transition-all duration-300 ml-16`}>
-        <Header />
-        {children}
-      </main>
-      {/* Botón de modo oscuro */}
-      <button
-        className="fixed bottom-4 right-4 p-2 bg-gray-200 dark:bg-gray-700 rounded-full shadow-lg z-50"
-        onClick={toggleModoOscuro}
-      >
-        {modoOscuro ? (
-          <Sun className="text-yellow-400" />
-        ) : (
-          <Moon className="text-gray-700" />
-        )}
-      </button>
-    </div>
+    <button
+      className="fixed bottom-4 right-4 p-2 bg-gray-200 dark:bg-gray-700 rounded-full shadow-lg z-50"
+      onClick={() => setTheme(old => old === "dark" ? "light" : "dark")}
+    >
+      {isDark ? (
+        <Sun className="text-yellow-400" />
+      ) : (
+        <Moon className="text-gray-700" />
+      )}
+    </button>
   );
 };
 
