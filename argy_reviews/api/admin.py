ac61968c7eb28299c722548427259acafcc54395
Facuterremoto <facuterremoto04@gmail.com>
2025-01-27 20:44:48 -0300

    ADmin lol

--- argy_reviews/api/admin.py
+++ argy_reviews/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 from django.contrib.contenttypes.models import ContentType
+from django.utils.html import format_html
+from django.urls import reverse
+
 
 # Filtro personalizado para ContentType
 class ContentTypeFilter(admin.SimpleListFilter):
@@ -23,17 +26,36 @@ class ContentTypeFilter(admin.SimpleListFilter):
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ('code', 'reporter', 'reported_object', 'category', 'resolved', 'created_at')
+    list_display = ('linked_reported_object','reporter',  'category', 'resolved', 'created_at')
     list_filter = (ContentTypeFilter, 'resolved', 'created_at')  # Agrega el filtro personalizado
     search_fields = ('code', 'reporter__username', 'content')  # Para buscar reportes específicos
     date_hierarchy = 'created_at'  # Navegación jerárquica por fechas
+    list_editable = ('resolved',)  # Hace editable el campo "resolved" directamente en la lista
+
+    def linked_reported_object(self, obj):
+        """Genera un enlace al objeto reportado en la interfaz de administrador."""
+        if obj.reported_object:
+            # Obtén el modelo y la URL del objeto reportado
+            content_type = obj.reported_content_type
+            object_id = obj.reported_object_id
+            admin_url = reverse(
+                f'admin:{content_type.app_label}_{content_type.model}_change',
+                args=[object_id]
+            )
+            return format_html('<a href="{}">{}</a>', admin_url, obj.reported_object)
+        return "N/A"
+
+    linked_reported_object.short_description = "Reported Object"  # Título para la columna en el admin
+
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ( 'title', 'id', 'owner', 'verification_state__name', 'created_at')
+    list_display = ('title', 'id', 'owner', 'verification_state__name', 'created_at')
     list_filter = ('verification_state__name', 'created_at')
     search_fields = ('code', 'owner__username')
     date_hierarchy = 'created_at'
+
 
 class StudentAdmin(admin.ModelAdmin):
     filter_horizontal = ('courses',)
