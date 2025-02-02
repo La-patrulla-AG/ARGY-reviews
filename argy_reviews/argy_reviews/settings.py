b702ba1d2ab1e9c8b3ffd037f7d98b9c84fe3134
Facuterremoto <facuterremoto04@gmail.com>
2025-02-01 21:40:58 -0300

    fix database

--- argy_reviews/argy_reviews/settings.py
+++ argy_reviews/argy_reviews/settings.py
@@ -143,16 +143,7 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     
-    #'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME', 'default_db_name'),
-        'USER': os.getenv('DATABASE_USER', 'default_user'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'default_password'),
-        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
-        'PORT': os.getenv('DATABASE_PORT', '5432'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     
 }
 
