5ff3ae76cc29c73ec4ca71c4fd0b371f53c29c61
Facuterremoto <facuterremoto04@gmail.com>
2025-02-09 05:52:36 -0300

    feat: enhance ProtectedRoute component with improved token validation and navigation handling

--- argy_reviews/frontend/src/components/ui/ProtectedRoute.jsx
+++ argy_reviews/frontend/src/components/ui/ProtectedRoute.jsx
@@ -1,21 +1,38 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
-//import { useMe } from "../hooks/useMe";
-import {jwtDecode} from "jwt-decode";
+import { Navigate, useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import api from "../../api/api";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../../api/constants";
 import { useState, useEffect } from "react";
 
 function ProtectedRoute({ children }) {
   const [isAuthorized, setIsAuthorized] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    auth().catch(() => setIsAuthorized(false))
+    auth().catch(() => setIsAuthorized(false));
   }, []);
 
   const refreshToken = async () => {
     const refreshToken = localStorage.getItem(REFRESH_TOKEN);
-    try{
+    if (!refreshToken) {
+      setIsAuthorized(false);
+      return;
+    }
+
+    const decodedRefresh = jwtDecode(refreshToken);
+    const refreshTokenExpiration = decodedRefresh.exp;
+    const now = Date.now() / 1000;
+
+    if (refreshTokenExpiration < now) {
+      // Si el refresh_token ha expirado, deslogueamos al usuario
+      localStorage.removeItem(ACCESS_TOKEN);
+      localStorage.removeItem(REFRESH_TOKEN);
+      setIsAuthorized(false);
+      return;
+    }
+
+    try {
       const response = await api.post("/token/refresh/", {
         refresh: refreshToken,
       });
@@ -30,13 +47,14 @@ function ProtectedRoute({ children }) {
       setIsAuthorized(false);
     }
   };
-  
+
   const auth = async () => {
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (!token) {
       setIsAuthorized(false);
       return;
     }
+
     const decoded = jwtDecode(token);
     const tokenExpiration = decoded.exp;
     const now = Date.now() / 1000;
@@ -49,10 +67,15 @@ function ProtectedRoute({ children }) {
   };
 
   if (isAuthorized === null) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
-  return isAuthorized ? children : <Navigate to="/" />; 
+  if (!isAuthorized) {
+    navigate("/");
+    return null;
+  }
+
+  return children;
 }
 
 export default ProtectedRoute;
