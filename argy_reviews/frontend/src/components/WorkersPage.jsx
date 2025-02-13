4b59543fb51a5ffec441463e2b039e64ac756ba0
Facuterremoto <facuterremoto04@gmail.com>
2025-02-13 07:31:58 -0300

    feat: actualizar URL de la API en la página de trabajadores

--- argy_reviews/frontend/src/components/WorkersPage.jsx
+++ argy_reviews/frontend/src/components/WorkersPage.jsx
@@ -7,9 +7,9 @@ const WorkersPage = () => {
         // Reemplaza esta URL con la URL correcta para obtener los datos del trabajador
         const fetchWorkerData = async () => {
             try {
-                const response = await axios.get("http://localhost:5000/app/interaccion/");
+                const response = await axios.get("http://localhost:8001/app/interaccion/");
                 const workerData = response.data;
-                setCvLink(`http://localhost:5000${workerData.cvlink}`);
+                setCvLink(`http://localhost:8001${workerData.cvlink}`);
             } catch (error) {
                 console.error("Error fetching worker data:", error);
             }
