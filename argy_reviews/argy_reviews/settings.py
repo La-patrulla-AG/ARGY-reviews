cfc9bd0848e81aa5c659947ba64e002c543ae857
Facuterremoto <facuterremoto04@gmail.com>
2025-02-13 13:11:25 -0300

    Eliminar WhiteNoise del middleware en la configuración de Django

--- argy_reviews/argy_reviews/settings.py
+++ argy_reviews/argy_reviews/settings.py
@@ -105,8 +105,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',   
-    'whiteNoise.middleware.WhiteNoiseMiddleware', 
+    'django.middleware.common.CommonMiddleware', 
 ]
 
 ROOT_URLCONF = 'argy_reviews.urls'
